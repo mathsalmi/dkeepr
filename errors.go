6091868a205a3c8c055b1327875669d8363454a3
Matheus Salmi <mathsalmi@gmail.com>
2015-12-04 09:05:51 -0200

    Adding error

--- errors.go
+++ errors.go
@@ -7,4 +7,5 @@ var (
 	ErrDriverNotSupported = errors.New("ORM: driver not supported")
 	ErrOpenConnection     = errors.New("ORM: error opening connection with DB")
 	ErrNotStruct          = errors.New("ORM: only structs can be persisted")
+	ErrConnNotOpen        = errors.New("ORM: connection not open")
 )
