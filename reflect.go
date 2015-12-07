20ffd052d7c14b6b11c86894a50b728d5a5e3288
Matheus Salmi <mathsalmi@gmail.com>
2015-12-07 21:07:16 -0200

    Adding support for native Interface() func

--- reflect.go
+++ reflect.go
@@ -4,6 +4,13 @@ import "reflect"
 
 // returns a value of a reflected object
 func getReflectedValue(t reflect.Kind, val reflect.Value) (value interface{}, err error) {
+	// try to use native methods
+	if val.CanInterface() {
+		return val.Interface(), nil
+	}
+
+	// try on hardcode if CanInterface fails.
+	// TODO: check safety of below operation
 	switch t {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		value = val.Int()
